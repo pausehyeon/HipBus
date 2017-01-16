856ec49be79b83e9db8324d94e008f9caf2dd709
pausehyeon <pausehyeon@gmail.com>
2017-01-16 15:11:44 +0900

    myBusEdit, myBusVideos 뷰, 핸들러 만들었어요

---
+++ HipBus/src/handler/mybus/MyBusEdit.java
@@ -0,0 +1,22 @@
+package handler.mybus;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import handler.CommandHandler;
+import handler.HandlerException;
+
+@Controller
+public class MyBusEdit implements CommandHandler{
+	
+	@RequestMapping("/myBusEdit.do")
+	@Override
+	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws HandlerException {
+		// TODO Auto-generated method stub
+		return new ModelAndView("myBusEdit");
+	}
+}
