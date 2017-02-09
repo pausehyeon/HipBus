18db6dce721eb0191fa78306f6c366841618166e
pausehyeon <pausehyeon@gmail.com>
2017-02-09 09:02:57 +0900

    클래스다이어그램에 맞추어 뷰와 핸들러 추가

---
+++ HipBus/src/handler/garage/GarageNewsModify.java
@@ -0,0 +1,23 @@
+package handler.garage;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import handler.CommandHandler;
+import handler.HandlerException;
+
+@Controller
+public class GarageNewsModify implements CommandHandler {
+
+	@RequestMapping("/garageNewsModify.do")
+	@Override
+	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws HandlerException {
+		// TODO Auto-generated method stub
+		return new ModelAndView("garageNewsModify");
+	}
+
+}
