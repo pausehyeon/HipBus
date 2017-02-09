3228caec8427baba31192b87b4a4a41825dd943d
nugulchang <nugulchang@naver.com>
2017-02-09 14:52:24 +0900

    2017.02.09 김지훈

---
+++ HipBus/src/model/MemberDto.java
@@ -0,0 +1,48 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class MemberDto {
+	
+	private String email;
+	private String passwd;
+	private String mem_level;
+	private Timestamp joindate;
+	private String imgname;	
+	
+	
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPasswd() {
+		return passwd;
+	}
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+	public String getMem_level() {
+		return mem_level;
+	}
+	public void setMem_level(String mem_level) {
+		this.mem_level = mem_level;
+	}
+	public Timestamp getJoindate() {
+		return joindate;
+	}
+	public void setJoindate(Timestamp joindate) {
+		this.joindate = joindate;
+	}
+	public String getImgname() {
+		return imgname;
+	}
+	public void setImgname(String imgname) {
+		this.imgname = imgname;
+	}
+	
+	
+
+}
