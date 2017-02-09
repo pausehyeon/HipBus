9d57b840802753c50d73d97f515c9190751e1110
pausehyeon <pausehyeon@gmail.com>
2017-02-09 09:17:21 +0900

    myBusBoard

---
+++ HipBus/src/model/mybus/MyBusBoardModifyResult.java
@@ -0,0 +1,23 @@
+package model.mybus;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import handler.CommandHandler;
+import handler.HandlerException;
+
+@Controller
+public class MyBusBoardModifyResult implements CommandHandler {
+
+	@RequestMapping("/myBusBoardModifyResult.do")
+	@Override
+	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws HandlerException {
+		// TODO Auto-generated method stub
+		return new ModelAndView("myBusBoardModifyResult");
+	}
+
+}
