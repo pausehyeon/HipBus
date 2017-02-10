4a69a5c19a5f60349e456469176385c498d82797
yoojaedeuk <yjd3728@nate.com>
2017-02-10 09:54:13 +0900

    DTo

--- HipBus/src/model/station/StationDBBean.java
+++ HipBus/src/model/station/StationDBBean.java
@@ -2,7 +2,11 @@ package model.station;
 
 import org.springframework.stereotype.Component;
 
+
+
+
 @Component("stationDao")
 public class StationDBBean implements StationDao {
-
+		
+	
 }
---
+++ HipBus/src/model/station/StationDataBean.java
@@ -0,0 +1,83 @@
+package model.station;
+
+import java.sql.Timestamp;
+
+public class StationDataBean {
+	private String nick;	//닉네임
+	private int num;		// 글번호
+	private Timestamp reg_date;	// 작성일
+	private int category;		// 카테고리
+	private String email;		// 이메일
+	private String subject;		// 글제목
+	private String content;		// 글내용
+	private int likenum;		// 추천수
+	private int readcount;		// 조회수
+	private Timestamp mod_date;	// 수정일
+	// 파일들은 아직안함
+	
+	
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public Timestamp getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Timestamp reg_date) {
+		this.reg_date = reg_date;
+	}
+	public int getCategory() {
+		return category;
+	}
+	public void setCategory(int category) {
+		this.category = category;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getLikenum() {
+		return likenum;
+	}
+	public void setLikenum(int likenum) {
+		this.likenum = likenum;
+	}
+	public int getReadcount() {
+		return readcount;
+	}
+	public void setReadcount(int readcount) {
+		this.readcount = readcount;
+	}
+	public Timestamp getMod_date() {
+		return mod_date;
+	}
+	public void setMod_date(Timestamp mod_date) {
+		this.mod_date = mod_date;
+	}
+	
+	
+	
+	
+}
