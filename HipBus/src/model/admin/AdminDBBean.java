a50b7bb1f4e9b10f7dff65740720cf3a7a66174e
KaHyeonNam <qltthfl18@gmail.com>
2017-02-10 09:58:59 +0900

    관리현황 정보 부름(Ddo 생성과 연계된 내용)

--- HipBus/src/model/admin/AdminDBBean.java
+++ HipBus/src/model/admin/AdminDBBean.java
@@ -1,9 +1,31 @@
 package model.admin;
 
 import org.springframework.stereotype.Component;
+import model.SqlMapClient;
 
 @Component("adminDao")
 public class AdminDBBean implements AdminDao {
 
+	//****관리자 현황****
+	@Override
+	public int reportMember() {
+		return SqlMapClient.getSession().selectOne("Admin.memberGrade");
+	}//멤버전체
+	@Override
+	public int reportGrade() {
+		return  SqlMapClient.getSession().selectOne("Admin.adminGrade");
+	}//관리자만
+	@Override
+	public int reportCrew() {
+		return SqlMapClient.getSession().selectOne("Admin.numberOfCrew");
+	}//크루전체
+	@Override
+	public int reportPost() {
+		return SqlMapClient.getSession().selectOne("Admin.numberOfPost"); 
+	}//게시물수
 
+	
+	//****멤버목록****
+	
+	
 }
--- HipBus/src/model/admin/AdminDao.java
+++ HipBus/src/model/admin/AdminDao.java
@@ -1,5 +1,12 @@
 package model.admin;
 
 public interface AdminDao {
+	//관리자현황
+	public int reportMember();		//전체멤버 수 불러오기
+	public int reportGrade();		//3등급관리자 수만 부르기
+	public int reportCrew();		//크루 수 전체
+	public int reportPost();		//게시글 수 전체
+	
+	//멤버 목록 부르기
 	
 }
