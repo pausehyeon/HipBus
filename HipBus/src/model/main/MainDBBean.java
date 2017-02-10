fb8f68d9edaab61cec680f02ddc8bacedefa1b28
nugulchang <nugulchang@naver.com>
2017-02-10 14:27:27 +0900

    17.02.10 김지훈

--- HipBus/src/model/main/MainDBBean.java
+++ HipBus/src/model/main/MainDBBean.java
@@ -1,8 +1,11 @@
 package model.main;
 
+import org.springframework.stereotype.Component;
+
 import model.MemberDto;
 import model.SqlMapClient;
 
+@Component( "MainDao" )
 public class MainDBBean implements MainDao{
 	
 	public int insertMember( MemberDto dto ) {
