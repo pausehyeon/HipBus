b2af15f27a6cbbfa4c3e34a421a2a9fa0b1fff88
nugulchang <nugulchang@naver.com>
2017-02-10 14:27:10 +0900

    17.02.10 김지훈

--- HipBus/src/handler/main/MainSignUpPro.java
+++ HipBus/src/handler/main/MainSignUpPro.java
@@ -1,23 +1,67 @@
 package handler.main;
 
+import java.io.UnsupportedEncodingException;
+import java.sql.Timestamp;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import handler.CommandHandler;
 import handler.HandlerException;
+import model.MemberDto;
+import model.main.MainDao;
 
 @Controller
 public class MainSignUpPro implements CommandHandler {
+	
+	@Autowired(required=false)
+	@Resource(name="MainDao")
+	public MainDao Maindao;
 
 	@RequestMapping("/mainSignUpPro.do")
 	@Override
 	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws HandlerException {
 		// TODO Auto-generated method stub
-		return new ModelAndView("mainSignUpPro");
+		
+		try {
+			request.setCharacterEncoding( "utf-8" );
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		MemberDto dto = new MemberDto();
+		dto.setEmail( request.getParameter("email"));
+		dto.setNick(request.getParameter("nick"));
+		dto.setPasswd( request.getParameter("passwd"));
+		dto.setJoindate(new Timestamp( System.currentTimeMillis() ));
+		
+		int result = Maindao.insertMember( dto );
+		
+		request.setAttribute("result", result);				
+		
+		return new ModelAndView("mainSignUpPro");		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
