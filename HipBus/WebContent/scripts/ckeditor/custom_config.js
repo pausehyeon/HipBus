0a479e2d974a6437ed6b08c7bee459aa47aca5da
pausehyeon <pausehyeon@gmail.com>
2017-02-12 19:58:48 +0900

    ckeditor에서 서버로 업로드하는 음원뿐아니라 유투브url도 embed할 수 있도록 함

--- HipBus/WebContent/scripts/ckeditor/custom_config.js
+++ HipBus/WebContent/scripts/ckeditor/custom_config.js
@@ -5,40 +5,30 @@ CKEDITOR.plugins.addExternal( 'widget', '/HipBus/scripts/ckeditor/plugins/widget
 CKEDITOR.plugins.addExternal( 'widgetselection', '/HipBus/scripts/ckeditor/plugins/widgetselection/', 'plugin.js' );
 CKEDITOR.plugins.addExternal( 'lineutils', '/HipBus/scripts/ckeditor/plugins/lineutils/', 'plugin.js' );
 CKEDITOR.plugins.addExternal( 'html5audio', '/HipBus/scripts/ckeditor/plugins/html5audio/', 'plugin.js' );
-CKEDITOR.plugins.addExternal( 'videodetector', '/HipBus/scripts/ckeditor/plugins/videodetector/', 'plugin.js' );
+CKEDITOR.plugins.addExternal( 'youtube', '/HipBus/scripts/ckeditor/plugins/youtube/', 'plugin.js' );
 
 
 CKEDITOR.editorConfig = function( config ) {
-	config.extraPlugins = 'widget';
-	config.extraPlugins = 'widgetselection';
-	config.extraPlugins = 'lineutils';
-	config.extraPlugins = 'videodetector';	//code 순서에 따라 둘 중 하나만 동작
-	config.extraPlugins = 'html5audio';
-	
+	config.extraPlugins = 'youtube,html5audio';
 	
 	config.toolbarGroups = [
-		//{ name: 'clipboard',   groups: [ 'clipboard', 'undo' ]},
-		//{ name: 'editing',     groups: [ 'find', 'selection', 'spellchecker' ] },
-		//{ name: 'links' },
-		//{ name: 'forms' },
-		//{ name: 'tools' },
-		{ name: 'document',	   groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
 		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-		{ name: 'styles' },
-		{ name: 'colors' },
-		{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align', 'bidi' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'about', groups: [ 'about' ] },
 		'/',
-		{ name: 'others' },
-		{ name: 'insert'}
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'insert', groups: [ 'insert', 'html5audio'] }
 	];
 
-	// Remove some buttons provided by the standard plugins, which are
-	// not needed in the Standard(s) toolbar.
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	config.removeButtons = 'Underline,Subscript,Superscript,Cut,Copy,Paste,PasteText,PasteFromWord,Scayt,Link,Anchor,Unlink,Table,Outdent,Indent,About,HorizontalRule,SpecialChar';
 
-	// Set the most common block elements.
-	config.format_tags = 'p;h1;h2;h3;pre';
-
-	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
+	
 };
