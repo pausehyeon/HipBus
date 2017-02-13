7a1ee2c5f1a2d3b505e0206ea87a2b48d0ca8e5c
KaHyeonNam <qltthfl18@gmail.com>
2017-02-13 10:34:24 +0900

    관리자 회원목록 불러오기 추가 -17.02.13

--- HipBus/src/model/admin/AdminDao.java
+++ HipBus/src/model/admin/AdminDao.java
@@ -1,5 +1,10 @@
 package model.admin;
 
+import java.util.List;
+import java.util.Map;
+
+import model.MemberDto;
+
 public interface AdminDao {
 	//관리자현황
 	public int reportMember();		//전체멤버 수 불러오기
@@ -8,5 +13,6 @@ public interface AdminDao {
 	public int reportPost();		//게시글 수 전체
 	
 	//멤버 목록 부르기
+	public List<MemberDto> getMemberLists(Map<String, Integer> map);
 	
 }
