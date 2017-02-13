7f919b2566cfd2093f3b28adb798cbd7e2c0b4e5
pausehyeon <pausehyeon@gmail.com>
2017-02-13 10:26:16 +0900

    mainDao 대소문자 수정

--- HipBus/src/handler/main/Main.java
+++ HipBus/src/handler/main/Main.java
@@ -1,5 +1,6 @@
 package handler.main;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -9,13 +10,26 @@ import org.springframework.web.servlet.ModelAndView;
 
 import handler.CommandHandler;
 import handler.HandlerException;
+import model.MemberDto;
+import model.main.MainDao;
 
 @Controller
 public class Main implements CommandHandler{
 	
+	@Resource(name="mainDao")
+	private MainDao dao;
+	
 	@RequestMapping("/main.do")
 	@Override
 	public ModelAndView process(HttpServletRequest request, HttpServletResponse response) throws HandlerException {
+		
+		String memEmail = (String) request.getSession().getAttribute("memEmail");
+		
+		if(memEmail != null){
+			MemberDto member = dao.getMember(memEmail);
+			request.setAttribute("member", member );			
+		}
+				
 		return new ModelAndView("main");
 	}
 }
