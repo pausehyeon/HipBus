d25b5de666ad50576def2a67226de601f30a9cf5
KaHyeonNam <qltthfl18@gmail.com>
2017-02-14 11:30:42 +0900

    회원관리목록 수정되어 변경 // imgname -> imgName 변수명 변경되어 수정함 - 17.02.14

--- HipBus/src/model/MemberDto.java
+++ HipBus/src/model/MemberDto.java
@@ -9,51 +9,69 @@ public class MemberDto {
 	private String passwd;		//비밀번호
 	private int mem_level;		//회원등급
 	private Timestamp joindate;	//가입일
-	private String imgname;		//이미지 파일명
+	private String imgName;		//이미지 파일명
 	private String imglocation;	//서버상 이미지 파일명
 	
+	//크루멤버의 크루아이디를 같이 부르기 때문에 추가
+	private String crewid;
+
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getNick() {
 		return nick;
 	}
 	public void setNick(String nick) {
 		this.nick = nick;
 	}
+
 	public String getPasswd() {
 		return passwd;
 	}
 	public void setPasswd(String passwd) {
 		this.passwd = passwd;
 	}
+
 	public int getMem_level() {
 		return mem_level;
 	}
 	public void setMem_level(int mem_level) {
 		this.mem_level = mem_level;
 	}
+
 	public Timestamp getJoindate() {
 		return joindate;
 	}
 	public void setJoindate(Timestamp joindate) {
 		this.joindate = joindate;
 	}
+
 	public String getImgName() {
-		return imgname;
+		return imgName;
 	}
-	public void setImgName( String imgname) {
-		this.imgname = imgname;
+	public void setImgName(String imgname) {
+		this.imgName = imgname;
 	}
+
 	public String getImglocation() {
 		return imglocation;
 	}
 	public void setImglocation(String imglocation) {
 		this.imglocation = imglocation;
 	}
+
+	public String getCrewid() {
+		return crewid;
+	}
+	public void setCrewid(String crewid) {
+		this.crewid = crewid;
+	}
+	
+	
 	
 	
 	
