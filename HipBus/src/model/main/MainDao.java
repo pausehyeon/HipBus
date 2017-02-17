46acc20dc350132a751ae0e5be9ca946a56ca0aa
nugulchang <nugulchang@naver.com>
2017-02-17 14:32:53 +0900

    2017.02.17 김지훈

--- HipBus/src/model/main/MainDao.java
+++ HipBus/src/model/main/MainDao.java
@@ -1,8 +1,8 @@
 package model.main;
 
 import java.util.List;
+import java.util.Map;
 
-import model.CrewDto;
 import model.MemberDto;
 import model.NewsDto;
 import model.TopDriversDto;
@@ -10,9 +10,11 @@ import model.UpcomingDto;
 
 public interface MainDao {
 	
+	public int emailValidate( Map<String, String> ms );
 	public int insertMember( MemberDto dto );
+	public int insertDriver( String email );
 	public List<TopDriversDto> getTopDrivers();
 	public List<NewsDto> getNewsArticles();
 	public List<UpcomingDto> getUpcomings();
-
+	public int emailCheck( Map<String, String> emailCheck );
 }
