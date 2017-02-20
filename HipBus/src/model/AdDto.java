b8fa061388e00404d0c744458b1845b5242d92a1
KaHyeonNam <qltthfl18@gmail.com>
2017-02-20 11:15:31 +0900

    AdDto 생성

---
+++ HipBus/src/model/AdDto.java
@@ -0,0 +1,26 @@
+package model;
+
+public class AdDto {
+	private int ad_num;
+	private String html;
+	private String imglocation;
+	
+	public int getAd_num() {
+		return ad_num;
+	}
+	public void setAd_num(int ad_num) {
+		this.ad_num = ad_num;
+	}
+	public String getHtml() {
+		return html;
+	}
+	public void setHtml(String html) {
+		this.html = html;
+	}
+	public String getImglocation() {
+		return imglocation;
+	}
+	public void setImglocation(String imglocation) {
+		this.imglocation = imglocation;
+	}
+}
