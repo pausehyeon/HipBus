58264b361201def495108857e667eca2da72a3e5
KaHyeonNam <qltthfl18@gmail.com>
2017-02-20 15:17:44 +0900

    크루목록 펑션추가

--- HipBus/WebContent/scripts/adminScript.js
+++ HipBus/WebContent/scripts/adminScript.js
@@ -24,3 +24,15 @@ function w3_close() {
 	mySidenav.style.display = "none";
 	overlayBg.style.display = "none";
 }
+//Accordion
+function myFunction(id) {
+	var x = document.getElementById(id);
+	if (x.className.indexOf("w3-show") == -1) {
+		x.className += " w3-show";
+		x.previousElementSibling.className += " w3-theme-d1";
+	} else {
+		x.className = x.className.replace("w3-show", "");
+		x.previousElementSibling.className = x.previousElementSibling.className
+				.replace(" w3-theme-d1", "");
+	}
+}
