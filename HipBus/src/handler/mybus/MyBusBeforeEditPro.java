74126ebdcb08807c3e4f365dd5b270a560437fff
KaHyeonNam <qltthfl18@gmail.com>
2017-02-28 10:46:50 +0900

    사용안하는 임포트만 제거

--- HipBus/src/handler/mybus/MyBusBeforeEditPro.java
+++ HipBus/src/handler/mybus/MyBusBeforeEditPro.java
@@ -1,8 +1,5 @@
 package handler.mybus;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
