1c08081041de1060d47871ad80df3e0fe8d3082a
nugulchang <nugulchang@naver.com>
2017-03-03 14:18:42 +0900

    잘못된 import 삭제

--- HipBus/src/handler/mybus/MyBusInsertChannelPro.java
+++ HipBus/src/handler/mybus/MyBusInsertChannelPro.java
@@ -4,7 +4,6 @@ import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.catalina.tribes.io.ChannelData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
