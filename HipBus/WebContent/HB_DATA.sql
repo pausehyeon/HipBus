773247de5bb63b35d4673ebcebcb202a52e1ed8d
yoojaedeuk <yjd3728@nate.com>
2017-03-06 14:22:12 +0900

    초기 data 수정

--- HipBus/WebContent/HB_DATA.sql
+++ HipBus/WebContent/HB_DATA.sql
@@ -1,13 +1,13 @@
 /* 관리자*/
-insert into hb_member values('admin@hipbus.com', '관리자1', 'a1a1a1', 1, sysdate, '');
+insert into hb_member values('admin@hipbus.com', 'Admin1', 'a1a1a1', 3, sysdate, 'AdminLogo.png');
 insert into hb_verification values('admin@hipbus.com', -1);
 
 /* 1등급 회원 */
-insert into hb_member values('aaa@email.com', 'AAA', 'a1a1a1', 1, sysdate, '');
-insert into hb_member values('bbb@email.com', 'BBB', 'a1a1a1', 1, sysdate, '');
-insert into hb_member values('ccc@email.com', 'CCC', 'a1a1a1', 1, sysdate, '');
-insert into hb_member values('ddd@email.com', 'DDD', 'a1a1a1', 1, sysdate, '');
-insert into hb_member values('eee@email.com', 'EEE', 'a1a1a1', 1, sysdate, '');
+insert into hb_member values('aaa@email.com', 'Jueun Jeong', 'a1a1a1', 1, sysdate, 'aaa.png');
+insert into hb_member values('bbb@email.com', 'Gahyeon Nam', 'a1a1a1', 1, sysdate, 'bbb.png');
+insert into hb_member values('ccc@email.com', 'Jihyeon Jeong', 'a1a1a1', 1, sysdate, 'ccc.png');
+insert into hb_member values('ddd@email.com', 'Victory Lee', 'a1a1a1', 1, sysdate, 'ddd.png');
+insert into hb_member values('eee@email.com', 'Jaeduek Yoo', 'a1a1a1', 1, sysdate, 'eee.png');
 
 insert into hb_driver values('aaa@email.com');
 insert into hb_driver values('bbb@email.com');
@@ -22,11 +22,11 @@ insert into hb_verification values('ddd@email.com', -1);
 insert into hb_verification values('eee@email.com', -1);
 
 /* 2등급 회원*/
-insert into hb_member values('fff@email.com', 'FFF', 'a1a1a1', 2, sysdate, '');
-insert into hb_member values('ggg@email.com', 'GGG', 'a1a1a1', 2, sysdate, '');
-insert into hb_member values('hhh@email.com', 'HHH', 'a1a1a1', 2, sysdate, '');
-insert into hb_member values('iii@email.com', 'III', 'a1a1a1', 2, sysdate, '');
-insert into hb_member values('jjj@email.com', 'JJJ', 'a1a1a1', 2, sysdate, '');
+insert into hb_member values('fff@email.com', 'Jihun Kim', 'a1a1a1', 2, sysdate, 'fff.png');
+insert into hb_member values('ggg@email.com', 'Jaehyeon Jo', 'a1a1a1', 2, sysdate, 'ggg.png');
+insert into hb_member values('hhh@email.com', 'Seongyong Moon', 'a1a1a1', 2, sysdate, 'hhh.png');
+insert into hb_member values('iii@email.com', 'Betrayer Moon', 'a1a1a1', 2, sysdate, 'iii.png');
+insert into hb_member values('jjj@email.com', 'Juda Moon', 'a1a1a1', 2, sysdate, 'jjj.png');
 
 insert into hb_driver values('fff@email.com');
 insert into hb_driver values('ggg@email.com');
